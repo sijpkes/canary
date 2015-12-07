8eac538940ab3d2b577d69e3f16d78ae8195c5ea
ps158 <ps158@newcastle.edu.au>
2015-12-07 13:51:28 +1100

    cleaning up

--- canaryBlogCheck.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $script = array("script" => file_get_contents("canarybc.js"),
-                   "base" => $_GET['base']);
-?>
-
-canaryBlogCheck(
-       <?= json_encode($script) ?>
-);
-
